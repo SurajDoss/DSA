31a9360e050d893daddad3aa75d6129d130b7a03
Suraj Doss <suraj_sebastien@yahoo.com>
2025-05-30 08:50:09 +0530

    heap - remove

--- src/Heaps/Heap.java
+++ src/Heaps/Heap.java
@@ -36,7 +36,67 @@ public class Heap {
         items[second] = temp;
     }
 
-    public void remove(){
+    public int remove(){
+        if(size == 0){
+            throw new IllegalStateException();
+        }
 
+        var valueToBeRemoved = items[0];
+        items[0] = items[--size];
+
+        var index = 0;
+        while( index <= size && !isValidParentint(index) ){
+            var largerChildIndex = getLargerChildIndex(index);
+
+            swap(index, largerChildIndex);
+            index = largerChildIndex;
+        }
+
+        return valueToBeRemoved;
+    }
+
+    private int getLargerChildIndex(int index) {
+        if(!hasLeftChild(index)){
+            return index;
+        }
+
+        if(!hasRightChild(index)){
+            return index;
+        }
+        return leftChild(index) > rightChild(index) ? leftChildIndex(index) : rightChildIndex(index);
+    }
+
+    private boolean hasLeftChild(int index){
+        return leftChildIndex(index)<= size;
+    }
+
+    private boolean hasRightChild(int index){
+        return rightChildIndex(index)<= size;
+    }
+    private boolean isValidParentint (int index){
+        if(!hasLeftChild(index)){
+            return true;
+        }
+
+        if (!hasRightChild(index))
+            return items[index] >= leftChild(index);
+
+        return  items[index] >= leftChild(index) && items[index] >= rightChild(index);
+    }
+
+    private int leftChild(int index){
+        return items[leftChildIndex(index)];
+    }
+
+    private int rightChild(int index){
+        return items[rightChildIndex(index)];
+    }
+
+    private int leftChildIndex(int index){
+        return index * 2 + 1;
+    }
+
+    private int rightChildIndex(int index){
+        return index * 2 + 2;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -69,5 +69,7 @@ public class Main {
         heap.insert(17);
         heap.insert(4);
         heap.insert(22);
+
+        heap.remove();
     }
 }
